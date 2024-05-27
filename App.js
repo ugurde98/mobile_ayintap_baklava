23b2b41c1c4dff78f8c0e892e0a6d59b83492b82
Mehmet KIYAK <65353042+mkiyak@users.noreply.github.com>
2024-05-27 12:05:43 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,40 @@
+import { WebView } from 'react-native-webview';
+import Constants from 'expo-constants';
+import { ActivityIndicator, Image, StyleSheet, View } from 'react-native';
+import { useEffect, useState } from 'react';
+
+export default function App() {
+
+  const [isLoading, setIsLoading] = useState(true); 
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  } ,[]);
+
+if(isLoading){
+
+  return(
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <Image source={require('./assets/splash.png')} style={{width:'100%',height:'100%'}}/>
+    </View>
+  )
+}
+
+  return (
+    <View style={{top:0, flex:1,backgroundColor:'#eae0cd'}}>
+    <WebView
+      style={styles.container}
+      source={{ uri: 'https://ayintapbaklava.com.tr/' }}
+    />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+});
